6b696194f5ea809b966a311d64659e0762dff4da
Guillaume Quittet <guillaume.quittet@gmail.com>
2017-06-20 16:48:48 +0200

    Switch to urxvt terminal

--- Configurations/awesome/rc.lua
+++ Configurations/awesome/rc.lua
@@ -46,7 +46,7 @@ end
 beautiful.init("/home/gquittet/.config/awesome/themes/my-theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xfce4-terminal"
+terminal = "urxvt"
 edit = "vim"
 editor = os.getenv("EDITOR") or edit
 editor_cmd = terminal .. " -e " .. editor
