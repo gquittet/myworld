2e801ffc1c51bb8eabc7906beba8242c4a2151ca
Guillaume Quittet <guillaume.quittet@gmail.com>
2017-07-24 21:34:04 +0200

    Change transparancy to 95%
    Change transparancy to 95%

--- Configurations/awesome/rc.lua
+++ Configurations/awesome/rc.lua
@@ -669,7 +669,7 @@ end)
 --end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus c.opacity=1 end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity=0.7 end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal c.opacity=0.85 end)
 -- }}}
 
 -- Startup programs
