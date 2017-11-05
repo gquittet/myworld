9b5396873cfedbcfa29a1e0fcac5a411bb5b5f05
Guillaume Quittet <guillaume.quittet@gmail.com>
2017-11-05 14:07:45 +0100

    Update vim and rofi

--- Configurations/i3/ThinkPad/scripts/rofi.sh
+++ Configurations/i3/ThinkPad/scripts/rofi.sh
@@ -19,7 +19,7 @@ rofi -modi "drun,window"                                                        
     -lines 7                                                                      \
     -padding 340                                                                  \
     -sidebar-mode                                                                 \
-    -color-window "argb:ee282a36, argb:ee282a36"                                  \
+    -color-window "argb:ee282a36, argb:ee282a36, #f8f8f2"                         \
     -color-normal "argb:ee282a36, #e6e6e6, argb:ee282a36, argb:eebd93f9, #f8f8f2" \
     -color-active "argb:ee282a36, #e6e6e6, argb:ee282a36, argb:eebd93f9, #f8f8f2" \
     -color-urgent "argb:ee282a36, #bd93f9, argb:ee282a36, argb:ee77003d, #bd93f9"
