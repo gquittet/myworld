2c9453bf7d1fce2445092eef993dff491b24d044
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-01-04 19:52:04 +0100

    Added app name + edit message

--- Scripts/check-email.sh
+++ Scripts/check-email.sh
@@ -5,6 +5,7 @@ maildirnew="$maildir/new/"
 maildirold="$maildir/cur/"
 
 icon="/usr/share/icons/Papirus-Dark/64x64/apps/evolution-mail.svg"
+appname="neomutt"
 
 offlineimap -o -u quiet
 
@@ -14,6 +15,6 @@ old=$(find $maildirold -type f | wc -l)
 if [ $new -gt 0 ]
 then
     title="$new new email!"
-    content="You've got new emails. Read it as soon as possible!"
-    notify-send -i "$icon" "$title" "$content"
+    content="Read them as soon as possible!"
+    notify-send -i "$icon" -a "$appname" "$title" "$content"
 fi
